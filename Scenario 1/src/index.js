e810737bd7f746e7ae08270bfe184a42e717fb1b
ranjana152 <ranjana.ravindran22@gmail.com>
2024-10-31 13:32:32 +0530

    Changes committed

--- Scenario 1/src/index.js
+++ Scenario 1/src/index.js
@@ -1,6 +1,8 @@
 
 function increaseNumber(initialValue, finalValue) {
-     //your code here
+     return finalValue-initialValue;
   }
-
+var finalValue=22;
+var initialValue=7;
+var result=increaseNumber(initialValue,finalValue);
 //Use SpecRunner to check the Test Cases.
--- Scenario 2/src/index.js
+++ Scenario 2/src/index.js
@@ -1,5 +1,14 @@
 function hero(bullets, dragons){
-     //your code here
-  }
-
+  bulletpower=dragons*2;
+  return bullets>=bulletpower;
+}
+var bullets=20;
+var dragons=5;
+var result=hero(bullets,dragons);
+if(result){
+  console.log("Maximum power is available!");
+}
+else{
+  console.log("Need more bullets!");
+}
 //Use SpecRunner to check the Test Cases.
